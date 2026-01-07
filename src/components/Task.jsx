8c1302692fa0fe8dbdeb35abc829e9ee28b15d07
macin-dev <ernesto.dev24@gmail.com>
2026-01-07 14:56:13 -0600

    feat(reusability): upgrade the Task and TaskList components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,13 +1,53 @@
-export default function Task({ icon, title, img }) {
+const taskStatesArr = {
+  completed: {
+    src: "/src/assets/Done_round_duotone.svg",
+    alt: "done round icon",
+    classNames: ["task-list__item--completed", "task-list__state--completed"],
+  },
+  progress: {
+    src: "/src/assets/Time_atack_duotone.svg",
+    alt: "time atack icon",
+    classNames: ["task-list__item--progress", "task-list__state--progress"],
+  },
+  canceled: {
+    src: "/src/assets/close_ring_duotone.svg",
+    alt: "close ring icon",
+    classNames: ["task-list__item--canceled", "task-list__state--canceled"],
+  },
+  uninitialized: {
+    classNames: [
+      "task-list__item--uninitialized",
+      "task-list__state--uninitialized",
+    ],
+  },
+};
+
+export default function Task({ icon, title, taskState, description }) {
+  const getStateColor = taskStatesArr[taskState]?.classNames;
+
   return (
-    <div className="task-list__item">
-      <span className="task-list__icon">{icon}</span>
+    <div className={`task-list__item ${getStateColor[0]}`}>
+      <span
+        className={`task-list__icon ${
+          description ? "task-list__icon--align-start" : null
+        }`}
+      >
+        {icon}
+      </span>
 
-      <h2 className="task-list__title">{title}</h2>
-
-      <div className="task-list__state">
-        <img src={img.src} alt={img.alt} />
+      <div className="task-list__texts">
+        <h2 className="task-list__title">{title}</h2>
+        {description && <p className="task-list__description">{description}</p>}
       </div>
+
+      {taskState != "uninitialized" && (
+        <div className={`task-list__state ${getStateColor[1]}`}>
+          <img
+            src={taskStatesArr[taskState]?.src}
+            alt={taskStatesArr[taskState]?.alt}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,16 +1,47 @@
 import Task from "./Task";
 
+const defaultTasks = [
+  {
+    id: 1,
+    title: "Task in Progress",
+    state: "progress",
+    icon: "⏰",
+    description: "",
+  },
+  {
+    id: 2,
+    title: "Task Completed",
+    state: "completed",
+    icon: "🏋🏻‍♂️",
+    description: "",
+  },
+  {
+    id: 3,
+    title: "Task Won't do",
+    state: "canceled",
+    icon: "☕️",
+    description: "",
+  },
+  {
+    id: 4,
+    title: "Task to do",
+    state: "uninitialized",
+    icon: "📚",
+    description: "Work on a Challenge on devChallenges.io, learn TypeScript.",
+  },
+];
+
 export default function TaskList() {
-  return (
-    <section className="task-list">
+  const tasksElements = defaultTasks.map((task) => {
+    return (
       <Task
-        icon="⏰"
-        title="Task in Progress"
-        img={{
-          src: "/src/assets/Time_atack_duotone.svg",
-          alt: "time duotone icon",
-        }}
+        icon={task.icon}
+        title={task.title}
+        taskState={task.state}
+        description={task.description}
       />
-    </section>
-  );
+    );
+  });
+
+  return <section className="task-list">{tasksElements}</section>;
 }
